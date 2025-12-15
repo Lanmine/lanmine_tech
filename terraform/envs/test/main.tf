5555994c934549051375b98bebe42a011b8bf947
hermanl0 <hermanl0@proton.me>
2025-12-15 20:30:51 +0100

    Add Terraform infrastructure and CI/CD workflow
    - Add Talos node module for Proxmox VM provisioning
    - Add GitHub Actions workflow with terraform plan/apply
    - Add SOPS-encrypted secrets for Proxmox and Talos configs
    - Add .gitignore to exclude tfstate and .terraform files

---
+++ terraform/envs/test/main.tf
@@ -0,0 +1,13 @@
+module "talos_test_node" {
+  source = "../../modules/talos-node"
+
+  providers = {
+    proxmox = proxmox.main
+  }
+
+  name        = "talos-test-01"
+  vmid        = 9201
+  target_node = "proxmox01"
+  vlan_tag    = 10
+  iso         = "local:iso/talos-v1.11.5-amd64.iso"
+}
---
+++ terraform/modules/talos-node/main.tf
@@ -0,0 +1,69 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source = "telmate/proxmox"
+    }
+  }
+}
+
+resource "proxmox_vm_qemu" "this" {
+  name        = var.name
+  vmid        = var.vmid
+  target_node = var.target_node
+
+  vm_state = "running"
+  qemu_os  = "l26"
+  bios     = "ovmf"
+  agent    = 1
+
+  memory = 4096
+
+  cpu {
+    cores   = 2
+    sockets = 1
+  }
+
+  scsihw = "virtio-scsi-single"
+  boot   = "order=ide2;scsi0"
+
+  # EFI disk for UEFI boot
+  efidisk {
+    storage = "local-lvm"
+    efitype = "4m"
+  }
+
+  # Disks
+  disks {
+    # Talos boot disk
+    scsi {
+      scsi0 {
+        disk {
+          storage = "local-lvm"
+          size    = "20"
+        }
+      }
+    }
+    # Talos ISO
+    ide {
+      ide2 {
+        cdrom {
+          iso = var.iso
+        }
+      }
+    }
+  }
+
+  # Network
+  network {
+    id     = 0
+    model  = "virtio"
+    bridge = "vmbr0"
+    tag    = var.vlan_tag
+  }
+
+  lifecycle {
+    ignore_changes = [
+      network,
+    ]
+  }
+}
